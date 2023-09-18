7b1b510a69a7ca27f411a3e4c0e904e629c72378
Rafal Kachniarz <rafal.kachniarz@unic.com>
2023-09-18 14:06:09 +0200

    roll20 now handling situations where you get both adv and dadv

--- src/Functions/helpers.js
+++ src/Functions/helpers.js
@@ -2,18 +2,23 @@ export function getRandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+export function xor(a, b) {
+  return a ? !b : b;
+}
+
 export function roll20(advantage = false, disadvantage = false) {
   const firstroll = getRandomNumber(1, 20);
-  const secondroll = getRandomNumber(1, 20);
-  if (disadvantage) return Math.min(firstroll, secondroll);
-  else if (advantage) return Math.max(firstroll, secondroll);
-  else return firstroll;
+
+  if (xor(advantage, disadvantage)) {
+    const secondroll = getRandomNumber(1, 20);
+    return advantage ? Math.max(firstroll, secondroll) : Math.min(firstroll, secondroll);
+  } else return firstroll;
 }
 
 export function pickRandom(objectArray) {
   var filler = 100 - objectArray.map((r) => r.chance).reduce((sum, current) => sum + current);
   if (filler <= 0) {
-    console.log('chances sum is higher than 100!');
+    console.error('Chances sum is higher than 100 in: ', objectArray);
     return;
   }
   var probability = objectArray
