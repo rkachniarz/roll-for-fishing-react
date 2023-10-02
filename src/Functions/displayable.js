04e59c1182cd85aa62185e49b5e99327f1d6cfe1
Rafal Kachniarz <rafal.kachniarz@unic.com>
2023-10-02 03:49:07 +0200

    modstate concept; sketching

--- src/Functions/displayable.js
+++ src/Functions/displayable.js
@@ -1,7 +1,8 @@
 import { defaultIcon } from '../../public/logo192.png';
 
 export class Displayable {
-  constructor(icon = defaultIcon, infotext = 'A thing') {
+  constructor(icon = defaultIcon, infotext = 'A thing', RMBFunction) {
     (this.icon = icon), (this.infotext = infotext);
+    this.RMBFunction = RMBFunction;
   }
 }
