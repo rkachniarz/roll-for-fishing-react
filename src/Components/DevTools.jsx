308b28e9799c7cb0d84657352990261fa9425815
Rafal Kachniarz <rafal.kachniarz@unic.com>
2023-10-16 15:24:53 +0200

    WIP sync

--- src/Components/DevTools.jsx
+++ src/Components/DevTools.jsx
@@ -3,9 +3,19 @@ import Modal from "./Modal";
 import Button from "./Button";
 import { useState } from "react";
 import Container from "./Container";
-import { Displayable } from "../Functions/displayable";
+import { Displayable, Item } from "../Functions/displayable";
+import defaultImage from "../Data/Assets/Item-default.png"
 
 const displayTest = new Displayable();
+const itemTest = new Item({
+  icon: defaultImage,
+  uid: 0,
+  name: 'Test Item',
+  description: 'An Item',
+  flavor: 'Nice and flavorful',
+  active: true,
+  itemMechanics: {awesome: true}
+})
 
 
 export default function DevTools(player){
@@ -13,17 +23,25 @@ let [modalOpen, setModalOpen] = useState(false)
 
 const displayThing =
     <Container cname="DisplayableItem">
-        <img className='Icon' src={displayTest.icon} alt={displayTest.infotext} onClick={displayTest.onClickFunction}></img>
+        <img className='Icon' src={displayTest.icon} alt={displayTest.description} onClick={displayTest.onClickFunction}></img>
     </Container>;
 
-const modalContent = 
+const displayItem =
+    <Container cname="DisplayableItem">
+        <img className='Icon' src={itemTest.icon} alt={itemTest.description} onClick={() => itemTest.onClickFunction({})}></img>
+    </Container>;
+
+const modalContent =
 <Container cname="App-main">
     <Container>
         <p>Siema</p>
         <Button cname="Button-small" callback={toggleModal}>Nara</Button>
     </Container>
     <Container cname="Inventory-outer CenterFlex">
-        <Container cname="Inventory-inner">{displayThing}{displayThing}{displayThing}{displayThing}{displayThing}{displayThing}{displayThing}</Container>
+        <Container cname="Inventory-inner">
+          {displayThing}{displayThing}{displayThing}{displayThing}
+          {displayItem}{displayItem}{displayItem}{displayItem}
+         </Container>
     </Container>
 </Container>;
 
--- src/Functions/displayable.js
+++ src/Functions/displayable.js
@@ -1,13 +1,38 @@
-import defaultIcon from '../Data/Assets/logo192.png';
+import defaultIcon from '../Data/Assets/defaultImage.png';
 
 function defaultOnClickFunction(){
   alert('This object has no onClick function defined');
-} 
+}
 
 export class Displayable {
-  constructor(icon = defaultIcon, infotext = 'A thing', onClickFunction=defaultOnClickFunction) {
+  constructor(icon = defaultIcon, description = 'A thing', flavor = '', onClickFunction=defaultOnClickFunction) {
     this.icon = icon;
-    this.infotext = infotext;
+    this.description = description;
+    this.flavor = flavor;
     this.onClickFunction = onClickFunction;
   }
+}
+
+export class Item extends Displayable {
+  constructor({uid, name, icon, description, flavor, active, itemMechanics}){
+    super(icon, description, flavor);
+    this.onClickFunction = this.toggleActive;
+    this.uid = uid;
+    this.name = name;
+    this.active = active; //bool
+    this.itemMechanics = itemMechanics;
+  }
+
+  toggleActive(modState){
+    if (this.active)
+      this.deactivate(modState);
+    else
+      this.activate(modState);
+
+    this.active = !this.active;
+  }
+
+  activate(modState){console.log("Item activated")}
+  deactivate(modState){console.log("Item deactivated")}
+
 }
