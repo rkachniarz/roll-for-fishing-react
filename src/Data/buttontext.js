290600dca260d119ad0a819919be7f87466c0167
Rafal Kachniarz <rafal.kachniarz@unic.com>
2023-12-03 01:21:57 +0100

    first draft of buttontext.js

---
+++ src/Data/buttontext.js
@@ -0,0 +1,37 @@
+export default buttontext_default = {
+  regular: ['Roll!', 'Get fishing!', 'Fish!', 'Cast the line!'],
+  treasure: ['Use new gear!', 'Celebrate the loot!'],
+  nothing: [],
+  nat1: [],
+  nat20: [],
+  junk: [],
+};
+
+export const buttontext_fighter = {
+  regular: ['Fight a fish!', 'Get figthting!'],
+  treasure: [],
+  nothing: [],
+  nat1: [],
+  nat20: [],
+  junk: [],
+};
+
+export const buttontext_mage = {
+  regular: ['Do some fishing magic!', 'Cast some magic!'],
+  treasure: [],
+  nothing: [],
+  nat1: [],
+  nat20: [],
+  junk: [],
+};
+
+export const buttontext_rogue = {
+  regular: ['Steal a fish!', 'Get some treasure!', 'Make money!'],
+  treasure: [],
+  nothing: [],
+  nat1: [],
+  nat20: [],
+  junk: [],
+};
+
+export const buttontext_events = {};
