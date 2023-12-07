7c1e2a80e3dbfd340c3211436ec54f6a7775d71e
Rafał Kachniarz <rafal.kachniarz@gmail.com>
2023-12-07 14:29:04 +0100

    added design draft of perks

--- src/Data/perks.js
+++ src/Data/perks.js
@@ -1,22 +1,40 @@
 // warrior
-// 3 beat up a fish that escaped and catch it (7 fish cooldown)
-// 6 fish with advantage. advantage from different sources now stacks/ crit on 18 and up 
-// 9 fight fish instead of trying to catch them
-// 
+// 1.active jump in and beat up a fish that escaped to catch it. cooldown 10
+// 3.passive: crit on 18 and up
+// 3.active: Increase your fishing skill by your level for next cast. cooldown: 10-level
+// 6.passive: fish with advantage. advantage from different sources now stacks (so you choose between n dice for n sources)
+// 6.active: Combine your fish find and your fishing skill for both rolls for next cast. cooldown: 10-level
+// 9.passive: Double your fishing skill. Double your fishing skill from treasure.
+// 9:active: Upgraded hero power: jump into the water, looking for fish to fight. For the next 5 turns, you immediately fight anything you find. Chance for underwater events.
+//
 // wizard
-// 3 sometimes find fish from other dimensions
-// 6 bend reality to increase the size and make a caught fish special
-// 9 simulacrum
-//  
+// 1.active: make your next fish find chance 100%. cooldown 10
+// 3.passive: sometimes find fish from other dimensions
+// 3.active: make sue your next fish is from the fishpool
+// 6.passive: Make hero power passive with cooldown 1
+// 6.active bend reality to increase the size and make a caught fish special
+// 9.passive: Fish find 100%. Rarely obtain treasure. Choose out of two fish when you catch fish.
+// 9 active: Choose 5 out of 10 fish. Immediately catch all of them.
+//
 // rogue
-// 3 gut fish to try and find treasure
-// 6 always get offered two choices of treasure
-// 9 ignore negative sides of treasures
-// 
+// 1.active: Gut caught fish to try and find junk/treasure. cooldown 10
+// 3.passive: Sometimes find Greedy fish, which are holding junk/treasure.
+// 3.active: Make sure your next catch is not a fish. cooldown 10
+// 6.passive Choose out of two treasures when you catch treasure. Hero power cooldown 3.
+// 6.active: The next time you'd catch junk, you get jewelery or treasure. cooldown 10
+// 9.passive: Ignore negative sides of treasures
+// 9.active: Gain bonuses to all rolls in the next 3 casts. The strength of the bonuses depend on how much wealth you acquire. cooldown 8
+
 // universal
-// a cow accompanies you. you can feed it weeds.
-// you now see gold value of everything. chance to fish out the elusive VENDORFISH
-// 
-// 
-// general perk: + 2n main stat / +n +n main and random other stat 
-// general perk: 
+// any_even: a cow accompanies you. you can feed it weeds.
+// any_even: double the chance of Events.
+// any_even: chance to find edible fish, which provide temporary bonuses
+// any_even: immediately find a piece of treasure from your current location
+// any_even: reroll your base stats twice. choose which of the three sets to keep.
+// any_even: you now see gold value of everything. chance to fish out the elusive vendorfish doubled.
+// any_even: you can now put any fish from your inventory into an aquarium
+// any_even: you can now have two fishing poles at the same time
+// any_even: + level main stat
+// any_even: + 0.5 level / + 0.5 level main and random other stat
+// 10: reset your level to 0. Empty your inventory. Start levelling your second class. You keep your current abilities.
+// 10: you can now level up to 99. some events only happen after level 10.
